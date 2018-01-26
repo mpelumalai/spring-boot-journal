95ebd7628e9bc16fd3f9de54a4ac16333a9e4355
Krishna <navkkrnair@gmail.com>
2018-01-26 21:44:47 +0530

    Changed to deploy in WildFly

--- src/main/java/com/spring/SprintBootWebApplication.java
+++ src/main/java/com/spring/SprintBootWebApplication.java
@@ -2,15 +2,22 @@ package com.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 @SpringBootApplication
-public class SprintBootWebApplication
-{
+public class SprintBootWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args)
-	{
-		SpringApplication.run(SprintBootWebApplication.class, args);
+	private static Class<SprintBootWebApplication> applicationClass = SprintBootWebApplication.class;
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(applicationClass);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(applicationClass, args);
 	}
 }
